aba9eae3ac4ee5ac7bb7dd82b046c9600506d8de
Lerpardo <ls6182315@gmail.com>
2022-10-07 11:09:18 -0300

    ajustes

--- src/tests/Table.test.js
+++ src/tests/Table.test.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { renderWithRouterAndRedux } from './helpers/renderWith';
-import Header from '../components/Header';
-import App from '../App';
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import { renderWithRouterAndRedux } from './helpers/renderWith';
+// import Header from '../components/Header';
+// import App from '../App';
